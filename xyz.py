7e7fba29de5b9d479750e0a2a4cf6fc590b9be92
souvikchtt <souvik.chtt1@gmail.com>
2020-09-18 19:41:39 +0530

    commit

--- xyz.py
+++ xyz.py
@@ -2,3 +2,4 @@ import pandas as pd
 col_lst=["emp_id","name","company","location","salary","dept_id"]
 vals=pd.read_csv("emp_hive.csv",usecols=col_lst)
 print(vals["name"].head(5))
+print(vals["salary"].max())
